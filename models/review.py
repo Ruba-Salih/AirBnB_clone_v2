7a10a2bc137fc763ef366b737e620b246cfac155
Ruba-Salih <rubasalih900@gmail.com>
2024-06-25 23:18:39 +0300

    Feature/Update-models

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 class named review that inharits from BaseModel
 """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
@@ -14,6 +15,7 @@ class Review(BaseModel, Base):
         text (string): The text of the review
 
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
