ca846735c23c858a68e9a398e721601f239589e2
Ruba-Salih <rubasalih900@gmail.com>
2024-07-31 16:30:36 +0300

    FixBug

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,7 +9,6 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -18,13 +17,12 @@ def closedb(exc):
     storage.close()
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes = False)
 def states_list():
     """ /states_list route """
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    storage.reload()
     app.run("0.0.0.0", 5000)
